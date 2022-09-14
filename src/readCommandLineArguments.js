ba9528bad70189748f41a90b5ee05923dac44cb5
saikiran-vennam <107356058+saikiran-vennam@users.noreply.github.com>
2022-09-14 23:11:09 +0530

    Delete src directory

--- src/readCommandLineArguments.js
+++
@@ -1,2 +0,0 @@
-console.log("Hello " + process.argv.slice(2, process.argv.length).join(" "));
-console.log("Hello " + process.argv[2]);
--- src/readEnvVariable.js
+++
@@ -1,2 +0,0 @@
-console.log("Hello " + process.env.USERNAME);
-console.log("Working in " + process.env.MyCompany);
